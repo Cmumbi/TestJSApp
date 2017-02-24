8a88e9b0beaee75322a61b396974bbaabed47b25
Cmumbi <bakhita650@gmail.com>
2017-02-24 12:30:23 +0300

    Create add student
    Create Add Student button.
    Create add student dialogue box.
    Create Save button.

--- WebApplication1/ScriptsNg/StudentController.js
+++ WebApplication1/ScriptsNg/StudentController.js
@@ -1,14 +1,13 @@
 ﻿app.controller("StudentCtrl", ["$scope", "StudentService",
 
-function ($scope, StudentService) {
+function ($scope, studentService) {
 
     var baseUrl = "http://localhost:50901/student/";
 
     $scope.getStudents = function () {
         var apiRoute = baseUrl + "GetStudents/";
-        var student = StudentService.getAll(apiRoute);
+        var student = studentService.getAll(apiRoute);
         student.then(function (response) {
-            console.log(response);
             $scope.students = response.data;
         },
         function (error) {
@@ -18,4 +17,37 @@ function ($scope, StudentService) {
     }
     $scope.getStudents();
 
+    $scope.addStudent = function () {
+        var getAction = $scope.Action;
+        var getData;
+        if (getAction === "Update") {
+            studentService.ID = $scope.studentId;
+            getData = studentService.updatestudent();
+            getData.then(function (msg) {
+                $scope.getStudents();
+                alert(msg.data);
+                $scope.divStudent = false;
+            }, function () {
+                alert("Error in updating record");
+            });
+        } else {
+
+            getData = studentService.addStudent();
+            getData.then(function (msg) {
+                $scope.getStudents();
+                alert(msg.data);
+                $scope.divStudent = false;
+            }, function () {
+                alert("Error in adding record");
+            });
+        }
+    }
+
+
+    $scope.addStudentDiv = function () {
+        $scope.Action = "Add";
+        $scope.divEmployee = true;
+        console.log($scope.Action);
+    }
 }]);
+
--- WebApplication1/ScriptsNg/StudentService.js
+++ WebApplication1/ScriptsNg/StudentService.js
@@ -3,5 +3,14 @@
     this.getAll = function(apiRoute) {
         urlGet = apiRoute;
         return $http.get(urlGet);
+    };
+    this.addStudent=function(student) {
+        var response = $http({
+            method: "post",
+            url: "baseUrl/AddStudents",
+            data: JSON.stringify(student),
+            dataType: "json"
+        });
+        return response;
     }
 });
